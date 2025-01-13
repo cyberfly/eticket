25baf6ea636ba777fcf8c4a5440581ae97a5580a
fathur rahman <cyberflyx@gmail.com>
2025-01-13 12:40:58 +0800

    sample ticket routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// jangan delete, routes untuk login logout register
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware(['auth'])->group(function () {
+    // tambah routes dalam ni kalau perlu login dulu
+
+    Route::get('/test', function() {
+        echo "Test route is working!";
+    });
+
+    Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+});
+
+
